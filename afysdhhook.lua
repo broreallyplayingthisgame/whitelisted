854e535aea272898c60544db013fc47302208040
broreallyplayingthisgame <98722729+broreallyplayingthisgame@users.noreply.github.com>
2022-06-05 20:48:47 -0700

    Update afysdhhook.lua

--- afysdhhook.lua
+++ afysdhhook.lua
@@ -1,11 +1,11 @@
 local afy
-afy = hookmetamethod(game, "__index", function(t, k)
-    if (t:IsA("Mouse") and (k == "Hit" or k == "Target") and Aiming.Check()) then
+afy = hookmetamethod(game,"__index",function(i,v)
+    if i:IsA("Mouse") and v == "Hit" or v == "Target" and Aiming.Check() then
         local SelectedPart = Aiming.SelectedPart
-        if (DaHoodSettings.SilentAim and (k == "Hit" or k == "Target")) then
-            local Hit = SelectedPart.CFrame + (SelectedPart.Velocity * DaHoodSettings.Prediction)
-            return (k == "Hit" and Hit or SelectedPart)
+        if DaHoodSettings.SilentAim and v == "Hit" or v == "Target" then
+            local Hit = SelectedPart.CFrame + SelectedPart.Velocity * DaHoodSettings.Prediction
+            return v == "Hit" and Hit or SelectedPart
         end
     end
-    return afy(t, k)
+    return afy(i,v)
 end)
